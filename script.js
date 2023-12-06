1ee35cafa927730f12ab75cd50f1cd7b1ad15004
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-12-06 21:58:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 function highlight() {
     //Write your code here
+	 var strongTags = document.querySelectorAll('strong');
 
+    // Apply CSS styles to each <strong> tag
+    for (var i = 0; i < strongTags.length; i++) {
+        strongTags[i].style.color = 'green'; // Example: Change text color to red
+
+    }
 
 }
 
 
 function return_normal() {
     //Write your code here
+	var strongTags = document.querySelectorAll('strong');
 
+    // Apply CSS styles to each <strong> tag
+    for (var i = 0; i < strongTags.length; i++) {
+        strongTags[i].style.color = 'black'; // Example: Change text color to red
+
+    }
     
 }
